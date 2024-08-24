0764bad698e51bd744539781162a950c60ac57b6
Victor Manuel <victormmartinezf@gmail.com>
2024-08-23 21:36:59 -0300

    subindo pastas com exercicios de estrutura de repetição for e while

---
+++ Aula 44/while-01.py
@@ -0,0 +1,10 @@
+# Estrutura While la usamos cuando no sabemos el número de veces que un programa necesita ser repetido
+
+while contraseña != "123456" or usuario != "Alejandra":
+  contraseña = input("Escriba su contraseña: ")
+  usuario = input("Escriba su usuario: ")
+  if(contraseña != "123456" or usuario != "Alejandra"):
+    print(f"Contraseña y usuarios incorrectos!")
+    
+    print("Conseguiste ingresar al sistema!")
+
---
+++ Aula 44/while-02.py
@@ -0,0 +1,7 @@
+# Contar del 1 al 10
+contador = 1
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+    
